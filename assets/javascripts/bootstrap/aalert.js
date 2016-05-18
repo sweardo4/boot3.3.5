9d57e4edc8ff6c29a910fb1b059e212b0b38b7c3
sweardo4 <337586346@qq.com>
2016-05-18 11:30:56 +0800

    alert

--- assets/javascripts/bootstrap/aalert.js
+++ assets/javascripts/bootstrap/aalert.js
@@ -1,51 +1,51 @@
-/**
- * Created by cui on 16-4-30.
- */
-+function ($) {
-    'use strict'
-    var dismiss = '[data-dismiss="alert"]'
-    var Alert = function(el){
-        $(el).on('click',dismiss,this.close)
++function($){
+
+  "use strict"
+  var dismiss = '[data-dismiss="alert"]'
+
+  var Alert = function(el){
+    $(el).on('click',dismiss,this.close)
+  }
+
+
+  Alert.prototype.close = function(e){
+    var $this = $(this);
+    var selector = $this.attr('data-target')
+    if (!selector) {//----------
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
     }
-    
-    Alert.prototype.close = function(e){
-        var $this = $(this)
-        var selector = $this.attr('data-target')
-        
-        if(!selector){
-            selector = $this.attr('href')
-            selector = selector && selector.replace(/.*(?=#[^\s]*$)/,'')
-        }
-        if(e) e.preventDefault()
-     
-        var $parent =$(selector)
-        $parent.trigger(e = $.Event('close.bs.alert'))
-           
-        if (!$parent.length) {
-            $parent = $this.closest('.alert')//closest jquery查找方法  从元素本身开始，逐级向上级元素匹配，并返回最先匹配的元素
-        }
-        
-        $parent.removeClass('in')
-        function removeElement(){
-            $parent.detach().trigger('closed.bs.alert').remove()
-        }
-        $.support.transition && $parent.hasClass('fade')?
-            $parent.one('bsTransitionEnd',removeElement)
-            .emulateTransitionEnd(Alert.TRANSITION_DURATION):removeElement()
+    var $parent = $(selector)
+    if(e) e.preventDefault()
+    if(!parent.length){
+      $parent = this.closest('.alert');
     }
-    
-    function Plugin(option){
-        return this.each(function(){
-            var $this =$(this)
-            var data = $this.data('bs.alert')
-            if(!data)$this.data('bs.alert',(data = new Alert(this)))
-            if(typeof option == 'string') data[option].call(this) 
-        })
-        
+    $parent = $($parent);
+    $parent.trigger(e = $.Event('close.bs.alert'))
+    function removeElement(){
+      $parent.detach().trigger('closed.bs.alert').remove()
     }
-    $.fn.alert    = Plugin
-    $.fn.alert.constructor = Alert
-    // $(dismiss)
-    $(document).on('click.bs.alert.data-api',dismiss,Alert.prototype.close)
+    removeElement();
+  }
+
+function Plugin(option){
+
+
+}
+
+  var old = $.fn.alert;
+
+  $.fn.alert = Plugin
+  $.fn.alert.Constructor = Alert
+
+  $.fn.alert.noConflict = function(){
+    $.fn.alert = old
+    return this
+  }
+
+  $(document).on('click.bs.alert.data-api',dismiss,Alert.prototype.close);
+
+
+
 
 }(jQuery)
--- assets/javascripts/bootstrap/alert.js
+++ assets/javascripts/bootstrap/alert.js
@@ -28,35 +28,39 @@
   Alert.prototype.close = function (e) {
 
     var $this    = $(this)
-    // console.log($this)
     var selector = $this.attr('data-target')
     //if 属性data-target存在  则不执行if  如果data-target 不存在则在href中取
-    // if (!selector) {//----------
-    //   selector = $this.attr('href')
-    //   selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
-    // }
+    if (!selector) {//----------
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+    }
     // 根据上面取得的选择器取得对应的jquery对象
     var $parent = $(selector)
     //阻止click的默认事件
-    // if (e) e.preventDefault()//submit表单  a标签 ---------
+    if (e) e.preventDefault()//submit表单  a标签 ---------
 
     //如果上面没能取到有效的元素，则选择第一个class含alert的祖先
     if (!$parent.length) {
       $parent = $this.closest('.alert')//closest jquery查找方法  从元素本身开始，逐级向上级元素匹配，并返回最先匹配的元素
     }
-    // 新建一个'close.bs.alert'事件，并用该事件触发上面所选到的jquery对象
-    // $parent.trigger(e = $.Event('close.bs.alert'))
+    // 新建一个'close.bs.alert'事件，并用该事件触发上面所选到的jquery对象 当调用 close 实例方法时立即触发该事件。
+    $parent.trigger(e = $.Event('close.bs.alert'))
     // 如果开发者利用preventDefault阻止了该事件的默认方法则return结束，否则继续执行
    // 注意：此处的e是新建的'close.bs.alert'事件，与上面被阻止默认的click事件无关
-    // if (e.isDefaultPrevented()) return
+    if (e.isDefaultPrevented()) return
 
-    // $parent.removeClass('in')
+    $parent.removeClass('in')
+
+
 
     function removeElement() {//detach 从DOM中删除所有匹配的元素。
       // detach from parent, fire event then clean up data
+      // 	当警告框被关闭时触发该事件（将等待 CSS 过渡效果完成）。
       $parent.detach().trigger('closed.bs.alert').remove()
     }
 
+
+    // fate 和in class 是关闭时的动画效果css类
     $.support.transition && $parent.hasClass('fade') ?
       $parent.one('bsTransitionEnd', removeElement)
           .emulateTransitionEnd(Alert.TRANSITION_DURATION) : removeElement()
@@ -78,7 +82,7 @@
   var old = $.fn.alert// 把命名空间里的原有的alert放入old
 
 
-  $.fn.alert             = Plugin
+  $.fn.alert             = Plugin  // 添加到jquery插件  直接这样使用$(selector).alert('')
   $.fn.alert.Constructor = Alert//重新设置插件的构造函数
 
 
@@ -96,7 +100,7 @@
 
   //  事件委托   触发dismiss时  关闭此元素
 
-  // 给click事件赋予命名空间click.bs.alert.data-api，
+  // 给click事件赋予命名空间click.bs.alert.data-api， 当删除指定事件时 只会删除这个命名空间下的这个  而不会删除其他事件
   $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
 
 }(jQuery);
